7051d8bf1b12c1d0cb2e60d278df3c931dd0393f
MATHIEU DUPLEIX <mdupleix@adgroupe.priv>
2018-05-29 15:53:26 +0200

    many longest_common_prefix algo merge two sorted list

---
+++ JavaScript/merge.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    if (n == 0) return;
+    while (m > 0 && n > 0) {
+        if (nums1[m - 1] > nums2[n - 1]) {
+            nums1[m + n - 1] = nums1[m - 1];
+            m--;
+        }
+        else {
+            nums1[m + n - 1] = nums2[n - 1];
+            n--;
+        }
+    }
+    while (n > 0) nums1[n - 1] = nums2[n-- - 1];
+    return nums1;
+};
+
+let nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3;
+console.log(merge(nums1, m, nums2, n));
