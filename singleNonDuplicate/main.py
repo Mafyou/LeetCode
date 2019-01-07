75068546237234af2b3b7a2ad574726cd7a267d7
Mafyou <mdupleix@hotmail.com>
2019-01-07 21:26:13 +0100

    little arrangement

--- singleNonDuplicate/main.py
+++ singleNonDuplicate/main.py
@@ -10,7 +10,6 @@ class Solution:
             print(tmpNums)
             if num not in tmpNums:
                 return num
-            ==
 
 toto = Solution()
 print(toto.singleNonDuplicate([1,1,2,3,3,4,4,8,8]))
